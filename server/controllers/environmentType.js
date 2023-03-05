8f63719bcf884dadb05098e270088fe758fa7256
solrac9212 <65933753+solrac9212@users.noreply.github.com>
2023-03-04 23:02:21 -0500

    update

--- server/controllers/environmentType.js
+++ server/controllers/environmentType.js
@@ -1,19 +1,22 @@
 const {
-  createEnvironmentType,updateEnvironmentType, getAllEnvironmentType, getEnvironmentTypeById  
-  } = require("../models/environmentType");
-  
+  createEnvironmentType,
+  updateEnvironmentType,
+  getAllEnvironmentType,
+  getEnvironmentTypeById,
+  createDesignTypeEnvironmentType,
+} = require("../models/environmentType");
 
-  const { subirArchivoImagen } = require("../helpers/subirarchivos");
+const { subirArchivoImagen } = require("../helpers/subirarchivos");
 
-  const path = require('path');
-  const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
 
-exports.createEnvironmentType = async(req, res) => {
- 
+exports.createEnvironmentType = async (req, res) => {
+  console.log("hola");
 
-    try {
-    const { EnvironmentTypeName  } = req.body
-    if (!EnvironmentTypeName ) {
+  try {
+    const { EnvironmentTypeName } = req.body;
+    if (!EnvironmentTypeName) {
       return res.status(400).json({
         status: false,
         err: {
@@ -21,7 +24,10 @@ exports.createEnvironmentType = async(req, res) => {
         },
       });
     }
-    const image = await subirArchivoImagen(req.files.EnvironmentTypeImage,"uploads/EnvironmentType");
+    const image = await subirArchivoImagen(
+      req.files.EnvironmentTypeImage,
+      "uploads/EnvironmentType"
+    );
     if (!image) {
       return res.status(400).json({
         status: false,
@@ -30,98 +36,94 @@ exports.createEnvironmentType = async(req, res) => {
         },
       });
     }
-  const  data= {
-    enviroment:{    EnvironmentTypeName,
-      EnvironmentTypeImage:image 
-      
-    }
-
-    
-  
-   
-      }
- const arr = req.body.designTypeEnvironmentType.map(type => ({ idDesignType: type }));
-
-data.designTypeEnvironmentType = arr;
-      const createdEnvironmentType = await createEnvironmentType(data);
-
-
-      res.json({
-        status: true,
-        data: createdEnvironmentType,
-      });
-    } catch (error) {
-      console.log(error);
-  
-      return res.status(400).json({
-        status: false,
-        err: {
-          message: 'No pudo ser el tipo de ambiente',
-        },
-      });
-    }
-  };
-
-
-  exports.updateEnvironmentType =async (req, res, next) => {
-    try {
-
-    // Desestructurar los campos del cuerpo de la petición
-    const { idEnvironmentType,EnvironmentTypeName  } = req.body
-    
-        
-    if (!idEnvironmentType||!EnvironmentTypeName) {
-      return res.status(400).json({ status: false, err: { message: "Datos de entrada incompletos" } });
-    }
-    const  data= {
-      idEnvironmentType:+idEnvironmentType,
+    const data = {
       EnvironmentTypeName,
-       }
+      EnvironmentTypeImage: image,
+    };
+
+    const createdEnvironmentType = await createEnvironmentType(data);
+
+    const arr = req.body.designTypeEnvironmentType
+      .split(",")
+      .map((type) => ({
+        EnvironmentType_idEnvironmentType:
+          createdEnvironmentType.idEnvironmentType,
+        DesignType_idDesignType: +type,
+      }));
+    const designTypeEnvironmentType = await createDesignTypeEnvironmentType(
+      arr
+    );
+    res.json({
+      status: true,
+      data: { createdEnvironmentType, designTypeEnvironmentType },
+    });
+  } catch (error) {
+    console.log(error);
+
+    return res.status(400).json({
+      status: false,
+      err: {
+        message: "No pudo ser el tipo de ambiente",
+      },
+    });
+  }
+};
+
+exports.updateEnvironmentType = async (req, res, next) => {
+  try {
+    // Desestructurar los campos del cuerpo de la petición
+    const { idEnvironmentType, EnvironmentTypeName } = req.body;
+
+    if (!idEnvironmentType || !EnvironmentTypeName) {
+      return res
+        .status(400)
+        .json({
+          status: false,
+          err: { message: "Datos de entrada incompletos" },
+        });
+    }
+    const data = {
+      idEnvironmentType: +idEnvironmentType,
+      EnvironmentTypeName,
+    };
     const environmentType = await getEnvironmentTypeById(data);
 
+    if (req.files && req.files.EnvironmentTypeImage) {
+      const image = await subirArchivoImagen(
+        req.files.EnvironmentTypeImage,
+        "uploads/EnvironmentType"
+      );
+      const filePath = path.join(
+        process.cwd(),
+        environmentType.EnvironmentTypeImage
+      );
+      fs.unlinkSync(filePath);
+      data.EnvironmentTypeImage = image;
+    }
 
-    console.log(environmentType,"hola");
-       if (req.files && req.files.EnvironmentTypeImage) {
-        const image = await subirArchivoImagen(req.files.EnvironmentTypeImage, "uploads/EnvironmentType");
-        const filePath = path.join(process.cwd(), environmentType.EnvironmentTypeImage);
-        fs.unlinkSync(filePath);
-        data.EnvironmentTypeImage = image;
-      }
-  
-      
     const result = await updateEnvironmentType(data);
     res.json({ status: true, data: result });
   } catch (error) {
     res.status(500).json({ status: false, error });
   }
-
 };
-  
 
-  exports.getAllEnvironmentType  = async (req, res) => {
-    try {
-      const allEnvironmentType = await getAllEnvironmentType();
-  
-   
-  
-      // Enviar la respuesta con los usuarios
-      res.json({
-        status: true,
-        data: allEnvironmentType,
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(500).send({
-        status: false,
+exports.getAllEnvironmentType = async (req, res) => {
+  try {
+    const allEnvironmentType = await getAllEnvironmentType();
 
-        message: 'No se pudo obtener las EnvironmentType',
-      });
-    }
-  };
+    // Enviar la respuesta con los usuarios
+    res.json({
+      status: true,
+      data: allEnvironmentType,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({
+      status: false,
+
+      message: "No se pudo obtener las EnvironmentType",
+    });
+  }
+};
 //sin uso
-
-
-
-
-
-
--- server/models/environmentType.js
+++ server/models/environmentType.js
@@ -1,107 +1,109 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-
 const createEnvironmentType = async (data) => {
 
-
-  console.log(data);
-  const { enviroment, ...updateData } = data;
-
-  console.log(enviroment,"Enviromet");
-  console.log(updateData.designTypeEnvironmentType,"sub");
-
-    try {
-      const result =  await prisma.environmentType.create({
-        enviroment,
-        DesignType_EnvironmentType:{
-          create:[
-            updateData.designTypeEnvironmentType
-          ]
-
-        }
-      })
+  try {
+    const result = await prisma.environmentType.create({
+      data,
+    });
 
 
-      console.log(result);
-  return result
-    } catch (e) {
-      return e
-    }finally {
-      // Siempre desconectar la base de datos después de la operación
-      await prisma.$disconnect();
-    }
-  };
 
-
-  const updateEnvironmentType= async (data) => {
-
- const { idEnvironmentType, ...updateData } = data;
-
-
- try {
-   // Actualizar usuario en la base de datos\
-   const result = await prisma.environmentType.update({
-     where: { idEnvironmentType },
-     data: updateData
-   });
-
-   // Llamar a la función de devolución de llamada con el resultado exitoso
-   return result
+    return result;
   } catch (e) {
 
-  console.log(e);
-   // Capturar excepción y llamar a la función de devolución de llamada con el error
-   return e
+    return e;
   } finally {
-   // Siempre desconectar la base de datos después de la operación
-   await prisma.$disconnect();
- }
+    // Siempre desconectar la base de datos después de la operación
+    await prisma.$disconnect();
+  }
 };
 
+const createDesignTypeEnvironmentType = async (data) => {
+  try {
+    const result = await prisma.designType_EnvironmentType.createMany({
+      data,
+    });
+
+
+
+    return result;
+  } catch (e) {
+
+    return e;
+  } finally {
+    // Siempre desconectar la base de datos después de la operación
+    await prisma.$disconnect();
+  }
+};
+
+const updateEnvironmentType = async (data) => {
+  const { idEnvironmentType, ...updateData } = data;
+
+  try {
+    // Actualizar usuario en la base de datos\
+    const result = await prisma.environmentType.update({
+      where: { idEnvironmentType },
+      data: updateData,
+    });
+
+    // Llamar a la función de devolución de llamada con el resultado exitoso
+    return result;
+  } catch (e) {
+    console.log(e);
+    // Capturar excepción y llamar a la función de devolución de llamada con el error
+    return e;
+  } finally {
+    // Siempre desconectar la base de datos después de la operación
+    await prisma.$disconnect();
+  }
+};
 
 const getAllEnvironmentType = async () => {
   try {
-    // Se llama a Prisma para buscar todos 
+    // Se llama a Prisma para buscar todos
     const result = await prisma.environmentType.findMany();
-    
+
     // Se cierra la conexión a Prisma
 
     // Se devuelve el resultado exitoso
     return result;
   } catch (e) {
     // En caso de error, se cierra la conexión a Prisma
-    
+
     // Se devuelve el error
     throw e;
-  }finally {
+  } finally {
     // Siempre desconectar la base de datos después de la operación
     await prisma.$disconnect();
   }
 };
 
 const getEnvironmentTypeById = async (data) => {
-
   const { idEnvironmentType } = data;
- 
- 
+
   try {
     // Actualizar usuario en la base de datos
     const result = await prisma.environmentType.findUnique({
       where: { idEnvironmentType },
     });
- 
+
     // Llamar a la función de devolución de llamada con el resultado exitoso
     return result;
   } catch (e) {
     // Capturar excepción y llamar a la función de devolución de llamada con el error
     throw e;
-
   } finally {
     // Siempre desconectar la base de datos después de la operación
     await prisma.$disconnect();
   }
- };
- 
- module.exports = { createEnvironmentType,updateEnvironmentType,getAllEnvironmentType,getEnvironmentTypeById };
+};
 
+module.exports = {
+  createEnvironmentType,
+  updateEnvironmentType,
+  getAllEnvironmentType,
+  getEnvironmentTypeById,
+  createDesignTypeEnvironmentType
+};
