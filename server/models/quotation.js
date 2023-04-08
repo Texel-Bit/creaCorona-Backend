2a0c69a1548078a837159896da68b4089efc0095
solrac9212 <65933753+solrac9212@users.noreply.github.com>
2023-04-07 22:53:09 -0500

    update

--- server/models/quotation.js
+++ server/models/quotation.js
@@ -2,115 +2,114 @@ const { PrismaClient } = require("@prisma/client");
 const { include } = require("underscore");
 const prisma = new PrismaClient();
 
-
 const createquotation = async (data) => {
+  try {
+    const result = await prisma.quotation.create({
+      data,
+    });
 
-    try {
-      const result =  await prisma.quotation.create({
-        data
-      })
-
-
-  return result
-    } catch (e) {
-
-        console.log(e);
-      return e
-    }finally {
-      // Siempre desconectar la base de datos después de la operación
-      await prisma.$disconnect();
-    }
-  };
-
-
-  const createQuotationProductDetails = async (data) => {
-
-    try {
-        const result = await prisma.quotationProductDetails.createMany({
-            data,
-          });
-
-
-  return result
-    } catch (e) {
-
-        console.log(e);
-      return e
-    }finally {
-      // Siempre desconectar la base de datos después de la operación
-      await prisma.$disconnect();
-    }
-  };
-
-  const createDesignColorshasquotation = async (data) => {
-
-    try {
-        const result = await prisma.designColors_has_quotation.createMany({
-            data,
-          });
-
-
-  return result
-    } catch (e) {
-
-        console.log(e);
-      return e
-    }finally {
-      // Siempre desconectar la base de datos después de la operación
-      await prisma.$disconnect();
-    }
-  };
-  const updatequotation = async (data) => {
-
- const { idquotation, ...updateData } = data;
-
- try {
-   // Actualizar usuario en la base de datos
-   const result = await prisma.quotation.update({
-     where: { idquotation },
-     data: updateData
-   });
-
-   // Llamar a la función de devolución de llamada con el resultado exitoso
-   return result
+    return result;
   } catch (e) {
-   // Capturar excepción y llamar a la función de devolución de llamada con el error
-   return e
+    console.log(e);
+    return e;
+  } finally {
+    // Siempre desconectar la base de datos después de la operación
+    await prisma.$disconnect();
+  }
+};
 
- } finally {
-   // Siempre desconectar la base de datos después de la operación
-   await prisma.$disconnect();
- }
+const createQuotationProductDetails = async (data) => {
+  try {
+    const result = await prisma.quotationProductDetails.createMany({
+      data,
+    });
+
+    return result;
+  } catch (e) {
+    console.log(e);
+    return e;
+  } finally {
+    // Siempre desconectar la base de datos después de la operación
+    await prisma.$disconnect();
+  }
+};
+
+const createDesignColorshasquotation = async (data) => {
+  try {
+    const result = await prisma.designColors_has_quotation.createMany({
+      data,
+    });
+
+    return result;
+  } catch (e) {
+    console.log(e);
+    return e;
+  } finally {
+    // Siempre desconectar la base de datos después de la operación
+    await prisma.$disconnect();
+  }
+};
+const updatequotation = async (data) => {
+  const { idquotation, ...updateData } = data;
+
+  try {
+    // Actualizar usuario en la base de datos
+    const result = await prisma.quotation.update({
+      where: { idquotation },
+      data: updateData,
+    });
+
+    // Llamar a la función de devolución de llamada con el resultado exitoso
+    return result;
+  } catch (e) {
+    // Capturar excepción y llamar a la función de devolución de llamada con el error
+    return e;
+  } finally {
+    // Siempre desconectar la base de datos después de la operación
+    await prisma.$disconnect();
+  }
 };
 
 const getAllQuotation = async () => {
   try {
     // Se llama a Prisma para buscar todos las compañias
-    const result = await prisma.quotation.findMany(
-  {include:{
-    DesignColors_has_quotation:true,
-    quotationProductDetails:true
-  }
+    const result = await prisma.quotation.findMany({
+      include: {
+        DesignColors_has_quotation: true,
+        quotationProductDetails: true,
 
-  }
-    );
-    
+        sysUser: {
+          select: {
+            userName:true,
+            lastName:true,
+            office: {
+              select: {
+                Company: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
     // Se cierra la conexión a Prisma
 
     // Se devuelve el resultado exitoso
     return result;
   } catch (e) {
     // En caso de error, se cierra la conexión a Prisma
-    
+
     // Se devuelve el error
     throw e;
-  }finally {
+  } finally {
     // Siempre desconectar la base de datos después de la operación
     await prisma.$disconnect();
   }
 };
 
-
-
-module.exports = { createquotation,getAllQuotation,createQuotationProductDetails,createDesignColorshasquotation};
-
+module.exports = {
+  createquotation,
+  getAllQuotation,
+  createQuotationProductDetails,
+  createDesignColorshasquotation,
+};
