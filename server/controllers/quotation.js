dc81c337bf425c8e6992319d6332a89e49309615
solrac9212 <65933753+solrac9212@users.noreply.github.com>
2023-05-22 21:20:25 -0500

    update

--- server/controllers/quotation.js
+++ server/controllers/quotation.js
@@ -16,9 +16,7 @@ const {
 } = require("../models/bundleCompanyPrice.js");
 const { subirArchivoImagen } = require("../helpers/subirarchivos");
 
-
 exports.createquotation = async (req, res) => {
-
   try {
     const token = req.get("JWT");
 
@@ -39,7 +37,7 @@ exports.createquotation = async (req, res) => {
       idbrecha,
       idFormatSizeTexture,
       quatitionArea,
-      
+
       // idbundleCompanyPrice
     } = req.body;
 
@@ -70,10 +68,8 @@ exports.createquotation = async (req, res) => {
       "uploads/quotation"
     );
 
-
-
     // Manejo de errores de subirArchivoImagen
-    if (!desingPatternImage||!simulationImage) {
+    if (!desingPatternImage || !simulationImage) {
       return res.status(400).json({
         status: false,
         err: {
@@ -97,14 +93,15 @@ exports.createquotation = async (req, res) => {
     const areaValdosa =
       (DesignTypeFormatSize.DesignTypeFormatSizeHeight *
         DesignTypeFormatSize.DesignTypeFormatSizeWidht) /
-        10000;
+      10000;
 
-      //redondear al nyumero mayor
-    const cantidadValdosas =Math.ceil(quatitionArea / areaValdosa) 
+    //redondear al nyumero mayor
+    const cantidadValdosas = Math.ceil(quatitionArea / areaValdosa);
 
     const bundle = await getBundleDesignTypeFormatSizeTexture(fortmatTexture);
 
-    const quotationPrice =areaValdosa * cantidadValdosas * bundle[0].bundleBasePrice;
+    const quotationPrice =
+      areaValdosa * cantidadValdosas * bundle[0].bundleBasePrice;
 
     const { companyZone_idcompanyZone, Company_idCompany } =
       await getAllOfficeByIdoffice(office);
@@ -124,7 +121,7 @@ exports.createquotation = async (req, res) => {
       await getBundleCompanyPriceByBundleCompanyTypeComopanyZone(
         bundleCompanyPrice
       );
-      console.log(10);
+    console.log(10);
 
     if (price == undefined) {
       return res.status(400).json({
@@ -139,9 +136,9 @@ exports.createquotation = async (req, res) => {
       customerName,
       customerLastname,
       customerEmail,
-      simulationImage:simulationImage,
-      desingPatternImage:desingPatternImage,
-      quatitionArea:+quatitionArea,
+      simulationImage: simulationImage,
+      desingPatternImage: desingPatternImage,
+      quatitionArea: +quatitionArea,
       customerPhoneNumber: customerPhoneNumber.toString(),
       quotationBundlePrice: +bundle[0].bundleBasePrice,
       quotationPrice: +quotationPrice,
@@ -166,69 +163,70 @@ exports.createquotation = async (req, res) => {
 
     console.log(11);
 
-  const createdquotation = await createquotation(data);
-  console.log(12);
-  const arrProductDetails = JSON.parse(req.body.quotationProductDetails);
-console.log(arrProductDetails,"13");
+    const createdquotation = await createquotation(data);
+    console.log(12);
+    const arrProductDetails = JSON.parse(req.body.quotationProductDetails);
+    console.log(arrProductDetails, "13");
 
-console.log(arrProductDetails,"arrProductDetails");
-  arrProductDetails.forEach((element, index) => {
-    console.log(element,"185");
+    console.log(arrProductDetails, "arrProductDetails");
+    arrProductDetails.forEach((element, index) => {
+      console.log(element, "185");
 
-    arrProductDetails[index].quotation_idquotation =
-      +createdquotation.idquotation;
-  });
-  console.log(13);
+      arrProductDetails[index].quotation_idquotation =
+        +createdquotation.idquotation;
+    });
+    console.log(13);
 
-  const createdQuotationProductDetails = await createQuotationProductDetails(
-    arrProductDetails
-  );
+    const createdQuotationProductDetails = await createQuotationProductDetails(
+      arrProductDetails
+    );
 
-  const arrDesignColorshasquotation = JSON.parse(req.body.DesignColors_has_quotation);
-  var arrDesignColorshasquotationFinal=[];
-  arrDesignColorshasquotation.forEach((element, index) => {
+    const arrDesignColorshasquotation = JSON.parse(
+      req.body.DesignColors_has_quotation
+    );
+    var arrDesignColorshasquotationFinal = [];
+    arrDesignColorshasquotation.forEach((element, index) => {
+      console.log(element, "196");
+      if (element.Design_idDesign != -1) {
+        arrDesignColorshasquotationFinal[index] =
+          arrDesignColorshasquotation[index];
+        arrDesignColorshasquotationFinal[index].quotation_idquotation =
+          +createdquotation.idquotation;
+      } else {
+      }
+    });
 
-    console.log(element,"196");
-if (element.Design_idDesign!=-1) {
-  arrDesignColorshasquotationFinal[index]=arrDesignColorshasquotation[index]
-  arrDesignColorshasquotationFinal[index].quotation_idquotation =
-  +createdquotation.idquotation;
-}else{
-}
-
-   
-  });
-
-
-  console.log(arrDesignColorshasquotationFinal,"202");
-  const createdDesignColorshasquotation =
-    await createDesignColorshasquotation(arrDesignColorshasquotationFinal);
+    console.log(arrDesignColorshasquotationFinal, "202");
+    const createdDesignColorshasquotation =
+      await createDesignColorshasquotation(arrDesignColorshasquotationFinal);
     createdquotation.cantidadValdosas = cantidadValdosas;
+    // res.json({
+    //   status: true,
+    //   data: createdquotation,
+    // });
+
+    data.cantidadValdosas = cantidadValdosas;
     res.json({
       status: true,
-      data: createdquotation,
+      data:createdquotation
     });
 
-  data.cantidadValdosas = cantidadValdosas;
-  res.json({
-    status: true,
-    data,
+    return res.status(200).json({
+      status: true,
+      message: "cotizacion creada correctametne",
+      data: createdQuotation,
+    });
+} catch (error) {
+  return res.status(400).json({
+    status: false,
+    error: {
+      message: "error to create the quotation",
+      data: error,
+    },
   });
-
-   
-   
-  } catch (error) {
-    return res.status(400).json({
-      status: false,
-      err: {
-        message: "No pudo ser creado la cotizacion msg 2 ",
-        data:error,
-      },
-    });
-  }
+}
 };
 exports.simulateQuotation = async (req, res) => {
-
   try {
     const token = req.get("JWT");
 
@@ -240,7 +238,6 @@ exports.simulateQuotation = async (req, res) => {
       idsysuser = 1;
     }
     const {
-    
       quotationWidth,
       quotationHeight,
       idbrecha,
@@ -249,12 +246,7 @@ exports.simulateQuotation = async (req, res) => {
       // idbundleCompanyPrice
     } = req.body;
 
-    if (
-      
-      !quatitionArea ||
-      !idbrecha ||
-      !idFormatSizeTexture
-    ) {
+    if (!quatitionArea || !idbrecha || !idFormatSizeTexture) {
       return res.status(400).json({
         status: false,
         err: {
@@ -262,10 +254,6 @@ exports.simulateQuotation = async (req, res) => {
         },
       });
     }
-
-
-    
-
 
     const fortmatTexture = {
       idFormatSizeTexture: +idFormatSizeTexture,
@@ -281,14 +269,15 @@ exports.simulateQuotation = async (req, res) => {
     const areaValdosa =
       (DesignTypeFormatSize.DesignTypeFormatSizeHeight *
         DesignTypeFormatSize.DesignTypeFormatSizeWidht) /
-        10000;
+      10000;
 
-      //redondear al nyumero mayor
-    const cantidadValdosas =Math.ceil(quatitionArea / areaValdosa) 
+    //redondear al nyumero mayor
+    const cantidadValdosas = Math.ceil(quatitionArea / areaValdosa);
 
     const bundle = await getBundleDesignTypeFormatSizeTexture(fortmatTexture);
 
-    const quotationPrice =areaValdosa * cantidadValdosas * bundle[0].bundleBasePrice;
+    const quotationPrice =
+      areaValdosa * cantidadValdosas * bundle[0].bundleBasePrice;
 
     const { companyZone_idcompanyZone, Company_idCompany } =
       await getAllOfficeByIdoffice(office);
@@ -320,8 +309,7 @@ exports.simulateQuotation = async (req, res) => {
     }
 
     const data = {
-    
-      quatitionArea:+quatitionArea,
+      quatitionArea: +quatitionArea,
       quotationBundlePrice: +bundle[0].bundleBasePrice,
       quotationPrice: +quotationPrice,
       quotationWidth: +quotationWidth,
@@ -344,21 +332,17 @@ exports.simulateQuotation = async (req, res) => {
     };
     console.log(1111);
 
-   
-  data.cantidadValdosas = cantidadValdosas;
-  res.json({
-    status: true,
-    data,
-  });
-
-   
-   
+    data.cantidadValdosas = cantidadValdosas;
+    res.json({
+      status: true,
+      data,
+    });
   } catch (error) {
     return res.status(400).json({
       status: false,
       err: {
         message: "No pudo ser creado la cotizacion msg 1",
-        data:error,
+        data: error,
       },
     });
   }
@@ -366,23 +350,27 @@ exports.simulateQuotation = async (req, res) => {
 exports.getAllQuotation = async (req, res) => {
   try {
     const allQuotation = await getAllQuotation();
-    
-    const updatedQuotation = await Promise.all(allQuotation.map(async (element) => {
-      const fortmatTexture = {
-        idFormatSizeTexture: +element.FormatSizeTexture_idFormatSizeTexture,
-      };
-      const { DesignTypeFormatSize } = await getFormatSizeTextureById(fortmatTexture);
-  
-      const areaValdosa =
-        (DesignTypeFormatSize.DesignTypeFormatSizeHeight *
-          DesignTypeFormatSize.DesignTypeFormatSizeWidht) /
-        10000;
-  
-      const cantidadValdosas = Math.ceil(element.quatitionArea / areaValdosa);
-  
-      return { ...element, cantidadValdosas };
-    }));
-    
+
+    const updatedQuotation = await Promise.all(
+      allQuotation.map(async (element) => {
+        const fortmatTexture = {
+          idFormatSizeTexture: +element.FormatSizeTexture_idFormatSizeTexture,
+        };
+        const { DesignTypeFormatSize } = await getFormatSizeTextureById(
+          fortmatTexture
+        );
+
+        const areaValdosa =
+          (DesignTypeFormatSize.DesignTypeFormatSizeHeight *
+            DesignTypeFormatSize.DesignTypeFormatSizeWidht) /
+          10000;
+
+        const cantidadValdosas = Math.ceil(element.quatitionArea / areaValdosa);
+
+        return { ...element, cantidadValdosas };
+      })
+    );
+
     // Enviar la respuesta con las cotizaciones actualizadas
     res.json({
       status: true,
@@ -395,6 +383,5 @@ exports.getAllQuotation = async (req, res) => {
     });
   }
 };
-
 
 //sin uso
