c7e16079e7eaa3635df0be0d9119c4ed4e3b6616
Manuel <mangarcia_901@hotmail.com>
2023-05-22 19:36:26 -0500

    Update quotation.js

--- server/controllers/quotation.js
+++ server/controllers/quotation.js
@@ -19,6 +19,186 @@ const { log } = require("console");
 exports.createquotation = async (req, res) => {
 
   console.log(req.body);
+
+
+  const token = req.get("JWT");
+
+  let {
+    user: { idsysuser, office_idoffice },
+  } = await promisify(jwt.verify)(token, process.env.SEED);
+
+  if (!idsysuser) {
+    idsysuser = 1;
+  }
+  const {
+    customerName,
+    customerLastname,
+    customerEmail,
+    customerPhoneNumber,
+    quotationWidth,
+    quotationHeight,
+    idbrecha,
+    idFormatSizeTexture,
+    quatitionArea,
+    demo
+    // idbundleCompanyPrice
+  } = req.body;
+
+  if (
+    !customerName ||
+    !customerLastname ||
+    !customerEmail ||
+    !customerPhoneNumber ||
+    !quatitionArea ||
+    !idbrecha ||
+    !idFormatSizeTexture||
+    !demo
+  ) {
+    return res.status(400).json({
+      status: false,
+      err: {
+        message: "Datos de entrada incompletos",
+      },
+    });
+  }
+  const simulationImage = await subirArchivoImagen(
+    req.files.simulationImage,
+    "uploads/quotation"
+  );
+  const desingPatterImage = await subirArchivoImagen(
+    req.files.simulationImage,
+    "uploads/quotation"
+  );
+
+  
+
+  // Manejo de errores de subirArchivoImagen
+  if (!desingPatterImage||!simulationImage) {
+    return res.status(400).json({
+      status: false,
+      err: {
+        message: "Error al subir la imagen",
+      },
+    });
+  }
+  const fortmatTexture = {
+    idFormatSizeTexture: +idFormatSizeTexture,
+  };
+  const { DesignTypeFormatSize } = await getFormatSizeTextureById(
+    fortmatTexture
+  );
+
+  const office = {
+    idoffice: office_idoffice,
+  };
+
+  const areaValdosa =
+    (DesignTypeFormatSize.DesignTypeFormatSizeHeight *
+      DesignTypeFormatSize.DesignTypeFormatSizeWidht) /
+      10000;
+
+    //redondear al nyumero mayor
+  const cantidadValdosas =Math.ceil(quatitionArea / areaValdosa) 
+
+  const bundle = await getBundleDesignTypeFormatSizeTexture(fortmatTexture);
+
+  const quotationPrice =areaValdosa * cantidadValdosas * bundle[0].bundleBasePrice;
+
+  const { companyZone_idcompanyZone, Company_idCompany } =
+    await getAllOfficeByIdoffice(office);
+
+  const company = {
+    idCompany: Company_idCompany,
+  };
+  const { companyType_idcompanyType } = await getCompanyById(company);
+
+  const bundleCompanyPrice = {
+    idcompanyZone: companyZone_idcompanyZone,
+    idbundle: bundle[0].idbundle,
+    idcompanyType: companyType_idcompanyType,
+  };
+
+  const { price, idbundleCompanyPrice } =
+    await getBundleCompanyPriceByBundleCompanyTypeComopanyZone(
+      bundleCompanyPrice
+    );
+
+  if (price == undefined) {
+    return res.status(400).json({
+      ok: false,
+      err: {
+        message:
+          "No pudo ser creado la cotizacion no existe bundleCompanyPrice",
+      },
+    });
+  }
+  const data = {
+    customerName,
+    customerLastname,
+    customerEmail,
+    simulationImage:simulationImage,
+    quatitionArea:+quatitionArea,
+    customerPhoneNumber: customerPhoneNumber.toString(),
+    quotationBundlePrice: +bundle[0].bundleBasePrice,
+    quotationPrice: +quotationPrice,
+    quotationWidth: +quotationWidth,
+    quotationHeight: +quotationHeight,
+    quotationDate: new Date(),
+    quotationCompanyPrice: price,
+    FormatSizeTexture: {
+      connect: { idFormatSizeTexture: +idFormatSizeTexture },
+    },
+    quotationStatus: {
+      connect: { idquotationStatus: 1 },
+    },
+    brecha: {
+      connect: { idbrecha: +idbrecha },
+    },
+    bundleCompanyPrice: {
+      connect: { idbundleCompanyPrice: +idbundleCompanyPrice },
+    },
+    sysUser: { connect: { idsysuser: +idsysuser } },
+  };
+
+  console.log("Dataaaaaaaaaa ",data);
+ 
+if (demo==2) {
+const createdquotation = await createquotation(data);
+console.log(createdquotation,"hola");
+const arrProductDetails = req.body.quotationProductDetails;
+
+arrProductDetails.forEach((element, index) => {
+  arrProductDetails[index].quotation_idquotation =
+    +createdquotation.idquotation;
+});
+
+const createdQuotationProductDetails = await createQuotationProductDetails(
+  arrProductDetails
+);
+
+const arrDesignColorshasquotation = req.body.DesignColors_has_quotation;
+arrDesignColorshasquotation.forEach((element, index) => {
+  arrDesignColorshasquotation[index].quotation_idquotation =
+    +createdquotation.idquotation;
+});
+
+const createdDesignColorshasquotation =
+  await createDesignColorshasquotation(arrDesignColorshasquotation);
+  createdquotation.cantidadValdosas = cantidadValdosas;
+  res.json({
+    status: true,
+    data: createdquotation,
+  });
+}else{
+data.cantidadValdosas = cantidadValdosas;
+res.json({
+  status: true,
+  data,
+});
+}
+ 
+
+/*
   try {
     const token = req.get("JWT");
 
@@ -206,6 +386,8 @@ console.log(createdquotation,"hola");
       },
     });
   }
+
+  */
 };
 exports.simulateQuotation = async (req, res) => {
 
