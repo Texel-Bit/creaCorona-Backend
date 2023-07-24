38288af3230fd408147fc70f9bafe9023d4a67aa
Manuel <mangarcia_901@hotmail.com>
2023-07-24 01:14:50 -0500

    Upload

--- server/controllers/formatSizeTexture.js
+++ server/controllers/formatSizeTexture.js
@@ -8,6 +8,7 @@ const { subirArchivoImagen } = require("../helpers/subirarchivos");
 const path = require('path');
 const fs = require('fs');
 
+const { chromium } = require('playwright');
 
 exports.createFormatSizeTexture = async (req, res) => {
   try {
@@ -124,4 +125,87 @@ exports.getAllFormatSizeTexture = async (req, res) => {
 };
 
 
+exports.castHtmlToPng = async (req, res) => {
+
+
+  const htmlContent = Buffer.from(req.body.svgsContent, 'base64').toString('utf8');
+  const BrowserWidth=req.body.width;
+  const BrowserHeight=req.body.height;
+
+  const browser = await chromium.launch();
+  const context = await browser.newContext({
+    viewport: {
+      width: BrowserWidth,
+      height: BrowserHeight,
+    },
+    
+  });
+  const page = await context.newPage();
+  
+  const html = `<html>
+                <body>
+                ${htmlContent}
+                </body>
+                </html>`;
+  
+                console.log(html);
+  await page.setContent(html);
+  
+  // Since you want to screenshot the full page, you might not need to set the viewport size
+  // but it's here in case you want to control the dimensions of the page.
+  await page.setViewportSize({ width: BrowserWidth, height: BrowserHeight });
+  
+  // take a screenshot of the full page
+  const screenshotBuffer = await page.screenshot({ path: 'screenshot.jpeg', type: 'jpeg', fullPage: true });
+  
+  await browser.close();
+  
+  const screenshotUrl = `data:image/png;base64,${screenshotBuffer.toString('base64')}`;
+  res.send(screenshotUrl);
+  
+
+/*
+  console.log("Servie");
+
+  
+  try {
+    
+
+    const htmlContent = Buffer.from(req.body.svgsContent, 'base64').toString('utf8');
+
+   
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+  // Establecer el tamaño de la ventana del navegador para que coincida con el contenido SVG
+    await page.setViewport({ width: req.body.width, height: req.body.height });
+
+     // Cargar el contenido SVG en el navegador
+     await page.setContent(`<html><body>${htmlContent}</body></html>`, { waitUntil: 'domcontentloaded' });
+
+     console.log(`<html><body>${htmlContent}</body></html>`);
+
+     
+     // Capturar una captura de pantalla del contenido SVG
+    const screenshotBuffer = await page.screenshot({ type: 'png' ,fullPage: true});
+
+    await browser.close();
+
+        // Convertir el buffer de la captura de pantalla a base64 URL
+    const screenshotUrl = `data:image/png;base64,${screenshotBuffer.toString('base64')}`;
+    res.send(screenshotUrl);
+    
+    
+
+  } catch (error) {
+    console.error(error," ERROOOR ");
+    res.status(500).send({
+      message: "No se pudo obtener las allFormatSizeTexture",
+    });
+  }
+
+  */
+
+};
 //sin uso
