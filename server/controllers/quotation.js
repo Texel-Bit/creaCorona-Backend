97489cb2a077733c9cdb467a6bad792168fa5cbb
Carlos Colmenares <carlosmanuelcolmenares@gmail.com>
2023-07-25 22:59:44 -0500

    update

--- server/controllers/quotation.js
+++ server/controllers/quotation.js
@@ -245,156 +245,6 @@ exports.createquotation = async (req, res) => {
 
     data.cantidadValdosas = cantidadValdosas;
 
-    return res.status(200).json({
-      status: true,
-      message: "cotizacion creada correctametne",
-      data: createdquotation,
-    });
-  } catch (error) {
-    return res.status(400).json({
-      status: false,
-      error: {
-        message: "error to create the quotation",
-        data: error,
-      },
-    });
-  }
-};
-exports.simulateQuotation = async (req, res) => {
-  try {
-    const token = req.get("JWT");
-    let {
-      user: { idsysuser, office_idoffice, office },
-    } = await promisify(jwt.verify)(token, process.env.SEED);
-
-    if (!idsysuser) {
-      idsysuser = 1;
-    }
-    let {
-      quotationWidth,
-      quotationHeight,
-      idFormatSizeTexture,
-      quatitionArea,
-      idstate,
-      idFormatSize,
-      // idbundleCompanyPrice
-    } = req.body;
-
-    console.log(req.body.idFormatSize);
-
-    if (!idFormatSizeTexture) {
-      if (req.body.idFormatSize) {
-        let defaultFormatSizeTexture = await getDefaultFormatSizeTextureById(
-          req.body.idFormatSize
-        );
-
-        idFormatSizeTexture = defaultFormatSizeTexture.idFormatSizeTexture;
-      }
-    }
-
-    if (!quatitionArea || !idFormatSizeTexture || !idstate) {
-      return res.status(400).json({
-        status: false,
-        err: {
-          message: "Datos de entrada incompletos",
-        },
-      });
-    }
-
-    const fortmatTexture = {
-      idFormatSizeTexture: +idFormatSizeTexture,
-    };
-    const { DesignTypeFormatSize } = await getFormatSizeTextureById(
-      fortmatTexture
-    );
-
-    const officeInfo = {
-      idoffice: office_idoffice,
-    };
-
-    const areaValdosa =
-      (DesignTypeFormatSize.DesignTypeFormatSizeHeight *
-        DesignTypeFormatSize.DesignTypeFormatSizeWidht) /
-      10000;
-    const currentSetting = await getCurrentSettings();
-
-    quatitionArea =
-      quatitionArea +
-      (quatitionArea * currentSetting[0].SystemSetupGarbagePercenttage) / 100;
-
-    //redondear al nyumero mayor
-    const cantidadValdosas = Math.ceil(quatitionArea / areaValdosa);
-
-    const bundle = await getBundleDesignTypeFormatSizeTexture(fortmatTexture);
-
-    var state;
-
-    if (office.Company_idCompany == 1) {
-      state = {
-        idstate,
-      };
-    } else {
-      state = {
-        idstate: office.state_idstate,
-      };
-    }
-    const { companyZone_idcompanyZone } = await getStateByIdState(state);
-    const PriceByBundlePrice = {
-      idBundle: bundle[0].idbundle,
-      companyZone_idcompanyZone,
-    };
-
-    const bundlePriceZone = await getBundlePriceByZone(PriceByBundlePrice);
-
-    const quotationPrice = Math.round(
-      cantidadValdosas * bundlePriceZone[0].price
-    );
-
-    const { Company_idCompany } = await getAllOfficeByIdoffice(officeInfo);
-
-    const company = {
-      idCompany: Company_idCompany,
-    };
-    const { companyType_idcompanyType } = await getCompanyById(company);
-
-    const bundleCompanyPrice = {
-      idcompanyZone: companyZone_idcompanyZone,
-      idbundle: bundle[0].idbundle,
-      idcompanyType: companyType_idcompanyType,
-    };
-
-    // const { price, idbundleCompanyPrice } =
-    //   await getBundleCompanyPriceByBundleCompanyTypeComopanyZone(
-    //     bundleCompanyPrice
-    //   );
-
-    // if (price == undefined) {
-    //   return res.status(400).json({
-    //     ok: false,
-    //     err: {
-    //       message:
-    //         "No pudo ser creado la cotizacion no existe bundleCompanyPrice",
-    //     },
-    //   });
-    // }
-
-    const data = {
-      quatitionArea: +quatitionArea,
-      bundlePrice: +bundlePriceZone[0].price,
-      quotationPrice: +quotationPrice,
-      quotationWidth: +quotationWidth,
-      quotationHeight: +quotationHeight,
-
-      // quotationCompanyPrice: price,
-
-      // bundleCompanyPrice: {
-      //   connect: { idbundleCompanyPrice: +idbundleCompanyPrice },
-      // },
-      sysUser: { connect: { idsysuser: +idsysuser } },
-    };
-
-    data.cantidadValdosas = cantidadValdosas;
-
     const pdfDoc = await PDFDocument.load(url);
 
     const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
@@ -571,6 +421,156 @@ exports.simulateQuotation = async (req, res) => {
 
     await emailSend.sendEmailActivationCode(data);
     console.log(13);
+
+    return res.status(200).json({
+      status: true,
+      message: "cotizacion creada correctametne",
+      data: createdquotation,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      status: false,
+      error: {
+        message: "error to create the quotation",
+        data: error,
+      },
+    });
+  }
+};
+exports.simulateQuotation = async (req, res) => {
+  try {
+    const token = req.get("JWT");
+    let {
+      user: { idsysuser, office_idoffice, office },
+    } = await promisify(jwt.verify)(token, process.env.SEED);
+
+    if (!idsysuser) {
+      idsysuser = 1;
+    }
+    let {
+      quotationWidth,
+      quotationHeight,
+      idFormatSizeTexture,
+      quatitionArea,
+      idstate,
+      idFormatSize,
+      // idbundleCompanyPrice
+    } = req.body;
+
+    console.log(req.body.idFormatSize);
+
+    if (!idFormatSizeTexture) {
+      if (req.body.idFormatSize) {
+        let defaultFormatSizeTexture = await getDefaultFormatSizeTextureById(
+          req.body.idFormatSize
+        );
+
+        idFormatSizeTexture = defaultFormatSizeTexture.idFormatSizeTexture;
+      }
+    }
+
+    if (!quatitionArea || !idFormatSizeTexture || !idstate) {
+      return res.status(400).json({
+        status: false,
+        err: {
+          message: "Datos de entrada incompletos",
+        },
+      });
+    }
+
+    const fortmatTexture = {
+      idFormatSizeTexture: +idFormatSizeTexture,
+    };
+    const { DesignTypeFormatSize } = await getFormatSizeTextureById(
+      fortmatTexture
+    );
+
+    const officeInfo = {
+      idoffice: office_idoffice,
+    };
+
+    const areaValdosa =
+      (DesignTypeFormatSize.DesignTypeFormatSizeHeight *
+        DesignTypeFormatSize.DesignTypeFormatSizeWidht) /
+      10000;
+    const currentSetting = await getCurrentSettings();
+
+    quatitionArea =
+      quatitionArea +
+      (quatitionArea * currentSetting[0].SystemSetupGarbagePercenttage) / 100;
+
+    //redondear al nyumero mayor
+    const cantidadValdosas = Math.ceil(quatitionArea / areaValdosa);
+
+    const bundle = await getBundleDesignTypeFormatSizeTexture(fortmatTexture);
+
+    var state;
+
+    if (office.Company_idCompany == 1) {
+      state = {
+        idstate,
+      };
+    } else {
+      state = {
+        idstate: office.state_idstate,
+      };
+    }
+    const { companyZone_idcompanyZone } = await getStateByIdState(state);
+    const PriceByBundlePrice = {
+      idBundle: bundle[0].idbundle,
+      companyZone_idcompanyZone,
+    };
+
+    const bundlePriceZone = await getBundlePriceByZone(PriceByBundlePrice);
+
+    const quotationPrice = Math.round(
+      cantidadValdosas * bundlePriceZone[0].price
+    );
+
+    const { Company_idCompany } = await getAllOfficeByIdoffice(officeInfo);
+
+    const company = {
+      idCompany: Company_idCompany,
+    };
+    const { companyType_idcompanyType } = await getCompanyById(company);
+
+    const bundleCompanyPrice = {
+      idcompanyZone: companyZone_idcompanyZone,
+      idbundle: bundle[0].idbundle,
+      idcompanyType: companyType_idcompanyType,
+    };
+
+    // const { price, idbundleCompanyPrice } =
+    //   await getBundleCompanyPriceByBundleCompanyTypeComopanyZone(
+    //     bundleCompanyPrice
+    //   );
+
+    // if (price == undefined) {
+    //   return res.status(400).json({
+    //     ok: false,
+    //     err: {
+    //       message:
+    //         "No pudo ser creado la cotizacion no existe bundleCompanyPrice",
+    //     },
+    //   });
+    // }
+
+    const data = {
+      quatitionArea: +quatitionArea,
+      bundlePrice: +bundlePriceZone[0].price,
+      quotationPrice: +quotationPrice,
+      quotationWidth: +quotationWidth,
+      quotationHeight: +quotationHeight,
+
+      // quotationCompanyPrice: price,
+
+      // bundleCompanyPrice: {
+      //   connect: { idbundleCompanyPrice: +idbundleCompanyPrice },
+      // },
+      sysUser: { connect: { idsysuser: +idsysuser } },
+    };
+
+    data.cantidadValdosas = cantidadValdosas;
 
     res.json({
       status: true,
