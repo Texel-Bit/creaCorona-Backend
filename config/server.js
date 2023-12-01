9b36303140ca79ea0e9615734e4c28995593278c
Manuel <mangarcia_901@hotmail.com>
2023-12-01 08:06:14 -0500

    Update server.js

--- config/server.js
+++ config/server.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser');
 const WebSocketSingleton = require('../middlewares/WebSocketSingleton');
 
 const startServer = (app) => {
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 9445;
   const server = app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
   });
