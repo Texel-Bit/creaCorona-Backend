5307a133f1a9d06438d29c7f8c533ce2136c10bd
Manuel <mangarcia_901@hotmail.com>
2023-12-01 08:04:51 -0500

    Update server.js

--- config/server.js
+++ config/server.js
@@ -21,7 +21,7 @@ module.exports = () => {
   const app = express();
 
   //app.use(helmet());
-  app.use(cors());
+  // app.use(cors());
   app.use(express.json({ limit: '50mb' }));
   //app.use(cookieParser());
   //app.use(csrf({ cookie: true }));
